1040069d1ac85b94b355a79b0312321b668d23e4
Dariusz Okonski <darek200180@gmail.com>
2021-05-14 00:41:08 +0100

    add additional sout

--- src/main/java/com/polishDraughts/App.java
+++ src/main/java/com/polishDraughts/App.java
@@ -2,6 +2,8 @@ package com.polishDraughts;
 
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Polish draughts");
+        System.out.println("Hello world");
     }
 }
